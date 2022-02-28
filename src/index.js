2fc86c6747cdcfa896b0bb6620e440047e2e515a
Jose <flores.jose2185@gmail.com>
2022-02-28 10:49:03 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React, { useState } from "react";
+import Form from "./Form";
+import DisplayData from "./DisplayData";
 
-ReactDOM.render(
-  <Form />,
-  document.getElementById('root')
-);
+function ParentComponent() {
+  // ...
+  return (
+    <div>
+      <Form
+        firstName={firstName}
+        lastName={lastName}
+        handleFirstNameChange={handleFirstNameChange}
+        handleLastNameChange={handleLastNameChange}
+      />
+      <DisplayData firstName={firstName} lastName={lastName} />
+    </div>
+  );
+}
+export default ParentComponent;
